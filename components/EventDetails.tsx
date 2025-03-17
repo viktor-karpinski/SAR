ea0dd774c3860649d0eb298a1a8d8b09b9bb5d3e
Viktor Karpinski <developer@viktorkarpinski.com>
2025-03-17 13:23:44 +0100

    EventDetails component

---
+++ components/EventDetails.tsx
@@ -0,0 +1,72 @@
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {
+    event: Object;
+}
+
+const EventDetails = ({event} : Props) => {
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={[styles.text, styles.heading]}>
+                    {event.address}
+                </Text>
+
+                <View style={styles.line}></View>
+
+                <Text style={styles.text}>
+                    {(event.description != undefined && event.description.length )> 0 ? event.description : 'nie je k dispozícii žiadny popis'}
+                </Text>
+            </View>
+
+            {event.till == null && <View style={[styles.status, event.status == 'active' && {backgroundColor: "#154FA1"}]}>
+                <Text style={styles.text}>
+                    Aktuálny Stav: {event.status != 'active' ? 'Buď Pripravení' : 'Zásah je v Akcii'}
+                </Text>
+            </View>}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        flexShrink: 1,
+
+        flexDirection: "column",
+
+        paddingBlock: 25,
+        paddingInline: 20,
+        backgroundColor: "#191919B3",
+        borderWidth: 2,
+        borderColor: "#154FA1",
+    }, 
+
+    heading: {
+        fontSize: 20,
+    }, 
+
+    text: {
+        fontSize: 18,
+        fontFamily: "Hammersmith",
+        color: "#ffffff"
+    },
+
+    line: {
+        width: "100%",
+        height: 1,
+        backgroundColor: "#4B4B4B",
+        marginBlock: 25,
+    },
+
+    status: {
+        width: "100%",
+        backgroundColor: '#47484880',
+        height: 50,
+        marginTop: 10,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
+
+export default EventDetails;
